e701e5fce606ac2390092dd8aef6188aa65b62cd
Amelia Riding <amelia.riding@sofology.co.uk>
2021-08-02 11:35:54 +0100

    ApolloClient setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 function App() {
+  const client = new ApolloClient ({
+    uri: 'https://graphql-pokemon2.vercel.app/'
+  });
   return (
     <div className="App">
       <h3>HELLO WORLD</h3>
