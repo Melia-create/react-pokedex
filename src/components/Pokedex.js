1fad1eaa04fb6ce95c9e3570683bb18374ea5fb3
Amelia Riding <amelia.riding@sofology.co.uk>
2021-08-10 13:35:09 +0100

    Setting up MockData

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,39 +1,47 @@
 import React from 'react';
-import { AppBar, Toolbar, Grid, Card, CardContent } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles'
+import { AppBar, Toolbar, Grid, Card, CardContent, CircularProgress } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import MockData from '../data/MockData';
 
-const useStyles = makeStyles ({
+const useStyles = makeStyles({
     pokedexContainer: {
         paddingTop: "20px",
         paddingLeft: "50px",
         paddingRight: "50px",
-        }
-})
+    },
+});
 
 const getPokemonCard = () => {
-    return(
+    return (
         <Grid item xs={4}>
             <Card>
                 <CardContent>HELLO</CardContent>
-                </Card>
+            </Card>
         </Grid>
-    )
+    );
 
-}
+};
 
 const Pokedex = () => {
     const classes = useStyles();
+
+    const [pokemonData, setPokemonData] = useState(MockData)
+
     return (
         <>
             <AppBar position="static">
                 <Toolbar />
             </AppBar>
-            <Grid container spacing={2} className={classes.pokedexContainer}>
-                {getPokemonCard()}
-                {getPokemonCard()}
-                {getPokemonCard()}
-                {getPokemonCard()}
-            </Grid>
+            {pokemonData ? (
+                <Grid container spacing={2} className={classes.pokedexContainer}>
+                    {getPokemonCard()}
+                    {getPokemonCard()}
+                    {getPokemonCard()}
+                    {getPokemonCard()}
+                </Grid>
+            ) : (
+                    <CircularProgress />
+                )}
         </>
     );
 }
